97e3c9f718682568c211138ce74cda0bfa9bb54b
Dennis Mitchell <CPUsyntax@gmail.com>
2018-11-11 00:10:01 -0500

    Lecture 8

---
+++ CompleteJavaScript/lecture8/script.js
@@ -0,0 +1,19 @@
+var firstName = 'CPU';
+console.log(firstName);
+
+var lastName = "Syntax";
+var age = 50;
+console.log(lastName);
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'teacher';
+console.log(job);
+
+var _3years = 3;
+var johnMark = 'John and Mark';
+var _if = 23;
