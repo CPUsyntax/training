d33c4e835ab7ff2e581c3c8329c2b0ec5ed60bbd
Dennis Mitchell <CPUsyntax@gmail.com>
2018-11-16 13:53:54 -0500

    Exercise 4

---
+++ CompleteWebDeveloper/exercise4.js
@@ -0,0 +1,9 @@
+var age = prompt("What is your age?");
+
+if (age < 18) {
+    alert("Sorry too young to drive");
+} else if (age === 18) {
+    alert("First year driver");
+} else if (age > 18) {
+    alert("Powering on enjoy the ride!")
+}
