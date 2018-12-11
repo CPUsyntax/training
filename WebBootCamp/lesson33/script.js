786bd79fb3be702e54e787b92ed2bf7ad3d7bce3
Dennis Mitchell <CPUsyntax@gmail.com>
2018-12-11 13:14:52 -0500

    document.write

--- WebBootCamp/lesson33/script.js
+++ WebBootCamp/lesson33/script.js
@@ -6,11 +6,3 @@ function randColor(){
   return '#' + (function co(lor){   
       return (lor +=     [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])  && (lor.length == 6) ?  lor : co(lor); })('');
 }
-// document.write("<html><body><p>Hello World</p></body></html>")
-document.open("text/html", "replace");
-var hello = "I love my BB"
-document.write("<h1>Hello World</h1>");
-document.write("<h1>Hello Everyone</h1>");
-document.write(2 + 2);
-// document.write(<p>hello</p>);
-document.close();
